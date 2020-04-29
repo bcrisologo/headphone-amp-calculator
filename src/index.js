8366282979bf40031f98f86a0b4fc90338fdfdf0
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-04-28 18:16:36 -0700

    Testing for printing power calculations

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// declaring default volume levels in db PL
+const impedance = 37;
+const sensitivity = 94;
+
 const safe_volume = 85;
 const moderate_volume = 100;
 const fairlyloud_volume = 110;
@@ -11,88 +13,55 @@ const painful_volume = 120;
 
 /* Functions for power calculations */
 function powerCalculation(sensitivity) {
-	var power_safe;
-	var power_moderate;
-	var power_fairlyloud;
-	var power_veryloud;
-	var power_painful;
+	let power_safe = 10**((safe_volume - sensitivity) / 10);
+	let power_moderate = 10**((moderate_volume - sensitivity) / 10);
+	let power_fairlyloud = 10**((fairlyloud_volume - sensitivity) / 10);
+	let power_veryloud = 10**((veryloud_volume - sensitivity) / 10);
+	let power_painful = 10**((painful_volume - sensitivity) / 10);
 
-	power_safe = 10**((safe_volume - sensitivity) / 10)
-	power_moderate = 10**((moderate_volume - sensitivity) / 10)
-	power_fairlyloud = 10**((fairlyloud_volume - sensitivity) / 10)
-	power_veryloud = 10**((veryloud_volume - sensitivity) / 10)
-	power_painful = 10**((painful_volume - sensitivity) / 10)
+	/* Calculations part
+	power_safe = 10**((safe_volume - sensitivity) / 10);
+	power_moderate = 10**((moderate_volume - sensitivity) / 10);
+	power_fairlyloud = 10**((fairlyloud_volume - sensitivity) / 10);
+	power_veryloud = 10**((veryloud_volume - sensitivity) / 10);
+	power_painful = 10**((painful_volume - sensitivity) / 10);
+	*/
 		
+	/*
+	return (
+		power_safe.toPrecision(2)
+	)
+	*/
 	return {
-		power_safe : power_safe,
-		power_moderate : power_moderate,
-		power_fairlyloud : power_fairlyloud,
-		power_veryloud : power_veryloud,
-		power_painful : power_painful
+		power_safe: power_safe(),
 	}
 }
 
-class HeadphoneSpecs extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			impedance: '',
-			sensitivity: ''
-		};
+const elemental = (
+	<h3>
+	 Safe volume is at: {powerCalculation(sensitivity)} dB SPL
+	</h3>
+);
 
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	}
-	
-	handleChange(event) {
-		// Need for multiple events
-		// const name = target.name;
-		// const value = event.target.value;
-		/*
-		this.setState({
-			[name]: value
-		});
-		*/
-	}
 
-	handleSubmit(event) {
-		// Need for multiple submits
-		event.preventDefault();
-	}
-
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-			  <label>
-			    <h3> Headphone Impedance (Ohms): </h3>
-			    <input 
-			      name="impedance"
-			      type="text" 						// need to be able to convert text to number
-			      ref="impedance"
-			      value={this.state.value}
-			      onChange={this.handleChange}
-			    />
-			  </label>
-			  <label>
-			    <h3> Headphone Sensitivity (dB SPL / mW): </h3>
-			    <input
-			      name="sensitivity"
-			      type="text"						// need to be able to convert text to number
-			      ref="sensitivity"
-			      value={this.state.value}
-			      onChange={this.handleChange}
-			    />
-			  </label>
-			  <input 
-			    type="submit"
-			    value="Submit"
-			  />
-			</form>
-			);
-	}
+/* Sample Code tested working
+function formatName(user) {
+	return user.firstName + ' ' + user.lastName;
 }
+
+const user = {
+	firstName: 'Harper',
+	lastName: 'Perez'
+};
+
+const element = (
+	<h1>
+	 Hello, {formatName(user)}!
+	</h1>
+);
+*/
 
 ReactDOM.render(
-	<HeadphoneSpecs />,
+	elemental,
 	document.getElementById('root')
 );
