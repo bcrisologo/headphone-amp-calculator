333bc7a93390a80278878fbac72117f62b281164
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-04-29 16:14:22 -0700

    Updated handleSubmit and handleChange events, added Submit buttons that alerts if entries are non-numbers

--- src/index.js
+++ src/index.js
@@ -14,11 +14,75 @@ const elemental = (
 */
 class Trial extends React.Component {
 
+	constructor(props) {
+		super(props);
+		this.state = { 
+			sensitivity: '', 
+			impedance: '',
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		let name = event.target.name;
+		let value = event.target.value;
+
+		// Checking if values are not numerical
+		// if (!Number(value)) {
+		//	alert("Entry must be numerical");
+		//}
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+
+		let impedance = this.state.impedance;
+		let sensitivity = this.state.sensitivity;
+
+		// Checks if entries submitted are not numerical
+		if(!Number(impedance) && !Number(sensitivity)) {
+			alert("Impedance and Sensitivity entries are not numbers");
+		}
+			else if(!Number(impedance)) {
+				alert("Impedance entry is not a number");
+			}
+			else if(!Number(sensitivity)) {
+				alert("Sensitivity entry is not a number");
+			}
+		else {
+			return;
+		}
+	}
+
 	render(){
 		return (
-			<div>
-			  <h1>Hellow There</h1>
-			</div>
+			<form onSubmit={this.handleSubmit}>
+			  <h1> Thanks for coming to the headphone amp page </h1><br />
+			  <h3> Headphone Impedance is {this.state.impedance} </h3><br />
+			  <h3> Headphone Sensitivity is {this.state.sensitivity} </h3><br />
+			  <p> Impedance: </p>
+			  <input
+			    name="impedance"
+			    type="text"
+			    id="impedance"
+			    onChange={this.handleChange}
+			  />
+			  <p> Sensitivity (db SPL / mW): </p>
+			  <input
+			    name="sensitivity"
+			    type="text"
+			    id="sensitivity"
+			    onChange={this.handleChange}
+			  />
+			  <br /> <br />
+			  <input type="submit" />
+			</form>
 		);
 	}
 }
@@ -52,7 +116,8 @@ function powerCalculation(sensitivity, volume_level) {
 
 // Printing out volumes array
 for(var index in volumes) {
-	document.write( index + " : " + volumes[index] + " dB SPL" + "<br />");
+	//document.write( index + " : " + volumes[index] + " dB SPL" + "<br />");
+	document.write(index + " : " + volumes[index] + " dB SPL" + "<br />");
 }
 
 // Printing out calculated power calculations from the volumes array
