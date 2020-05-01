1ee0111f92b7510770eecd31418bbf3d542aa616
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-05-01 00:57:57 -0700

    Adding a third class for testing Submit handler

--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
-var volumes = {
-	safe_volume : 85,
-	moderate_volume : 100,
-	fairlyloud_volume : 110,
-	veryloud_volume : 115,
-	painful_volume : 120
+// Array for target volumes in dB SPL
+const volumes = {
+	safe_volume: 85,
+	moderate_volume: 100,
+	fairlyloud_volume: 110,
+	veryloud_volume: 115,
+	painful_volume: 120
 };
-
-// Test inputs
-// const impedance = 37;
-// const sensitivity = 94;
+const { safe_volume, moderate_volume, fairlyloud_volume, veryloud_volume, painful_volume } = volumes;
 
 // Base power calculator function
 function powerCalculation(sensitivity, volume_level) {
-	return 10**((volume_level - sensitivity) / 10);
+	return 10**((volume_level - sensitivity) / 10)
 }
 
 
@@ -26,19 +24,19 @@ class Trial extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = { 
-			sensitivity: '', 
-			impedance: '',
+			sensitivity: null, 
+			impedance: null,
 			errormessageone: '',
-			errormessagetwo: ''
+			errormessagetwo: '',
 		};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-	powerCalculation(sensitivity, volume_level) {
-		return 10**((volume_level - sensitivity) / 10);
-	}
+	// powerCalculation(sensitivity, volume_level) {
+	// 	return 10**((volume_level - sensitivity) / 10);
+	// }
 
 	handleChange(event) {
 		let name = event.target.name;
@@ -49,26 +47,24 @@ class Trial extends React.Component {
 
 		// Displays an error on the side of the input box
 		// when the entry is non-numerical
-		// Displays error for Impedance
+		// *** Can use ? operator when optimizing the IF statements
 		if(name === "impedance") {
-			if(value != "" && !Number(value)) {
+			if(value !== "" && !Number(value)) {
+				// Displays error for Impedance
 				errone = "  Impedance must be a number";
 			}
 		}
-		// Displays error for Sensitivity
 		if(name === "sensitivity") {
-			if(value != "" && !Number(value)) {
+			if(value !== "" && !Number(value)) {
+				// Displays error for Sensitivity
 				errtwo = "  Sensitivity must be a number";
 			}
 		}	
 
 		this.setState({
+			[name]: value,
 			errormessageone: errone,
 			errormessagetwo: errtwo
-		});
-
-		this.setState({
-			[name]: value
 		});
 	}
 
@@ -80,16 +76,17 @@ class Trial extends React.Component {
 
 		// Checks if entries submitted are not numerical
 		// Returns an alert message
-		if(impedance != '' && sensitivity != '' && 
+		// Can use ? operator when optimizing
+		if(impedance !== '' && sensitivity !== '' && 
 			!Number(impedance) && !Number(sensitivity)) {
 			// Both entries are non-numbers
 			alert("Impedance and Sensitivity entries are not numbers");
 		}
-			else if(impedance != '' && !Number(impedance)) {
+			else if(impedance !== '' && !Number(impedance)) {
 				// Impedance is not a number
 				alert("Impedance entry is not a number");
 			}
-			else if(sensitivity != '' && !Number(sensitivity)) {
+			else if(sensitivity !== '' && !Number(sensitivity)) {
 				// Sensitivity is not a number
 				alert("Sensitivity entry is not a number");
 			}
@@ -97,30 +94,23 @@ class Trial extends React.Component {
 				// Empty field
 				return;
 			}
-		else {
-			// Continue with no errors
-			return;
-		}
-
-
+		
 		// Need to include continuing function here
-
+		// alert(powerCalculation(this.state.sensitivity, volumes.safe_volume).toPrecision(2) + ' mW');
+		this.setState(state => ({
+			impedance: this.state.impedance,
+			sensitivity: this.state.sensitivity
+		}));
 	}
 
 	render(){
-
-		//const {
-		//	impedance,
-		//	sensitivity
-		//} = this.state;
-
 		return (
 			<div className="initialpage">
 			<form onSubmit={this.handleSubmit}>
 			  <h1> Thanks for coming to the headphone amp calculator page </h1><br />
 			  <h3> Headphone Impedance is {this.state.impedance} </h3><br />
 			  <h3> Headphone Sensitivity is {this.state.sensitivity} </h3><br />
-			  <p> Impedance: </p>
+			  <p> Impedance (Ohms): </p>
 			  <input
 			  	name="impedance"
 			    className="impedance"
@@ -141,11 +131,21 @@ class Trial extends React.Component {
 			  />
 			  {this.state.errormessagetwo}
 			  <br /> <br />
-			  <button type="submit">Enter</button>
+			  <button 
+			    type="submit"
+			    className="submit"
+			    >Enter
+			  </button>
 			  <br /><br />
 			</form>
 			  <div>
-			    <PowerOutput />
+			    <PowerOutput 
+			      sensitivity={this.state.sensitivity} 
+			      impedance={this.state.impedance}
+			      />
+			  </div>
+			  <div>
+			    <Results />
 			  </div>
 			</div>
 		);
@@ -158,10 +158,28 @@ class PowerOutput extends React.Component {
 		return(
 			<div className="powerresults">
 			<h3>This is rendered by PowerOutput class</h3>
+			<p>The impedance is {this.props.impedance}</p>
+			<br />
+			<p>The sensitivity is {this.props.sensitivity}</p>
 			</div>
 		)
 	}
 }
+
+class Results extends React.Component {
+	render() {
+		return (
+			<div>
+			
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<Trial />,
+	document.getElementById('root')
+);
 
 /* Volume sets at constant
 const safe_volume = 85;
@@ -181,9 +199,3 @@ const painful_volume = 120;
 // for(var index in volumes) {
 //	document.write( powerCalculation(sensitivity, volumes[index]).toPrecision(3) + " mW" + "<br />" );
 //}
-
-
-ReactDOM.render(
-	<Trial />,
-	document.getElementById('root')
-);
