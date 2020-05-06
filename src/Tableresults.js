8e8c5e00bc888629b869f38830fb9a44fdaf0eb5
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-05-05 17:20:19 -0700

    handleSubmit now updates all values on Tableresults

--- src/Tableresults.js
+++ src/Tableresults.js
@@ -1,20 +1,59 @@
 import React from "react";
+import "./index.css"
 
 class Tableresults extends React.Component {
 	render() {
 		return(
 			<div className="tableresults">
 			<h3>This is the Tableresults class</h3>
-			<p>{this.props.powerresults}</p>
-			<table id="resultstable">
-			  <thead>
-				<tr>
+			  <table className="tableouter">
+			    <thead>
+				  <tr>
 			  		<th>Category</th>
 			  		<th>Loudness (db SPL)</th>
 			  		<th>Voltage Needed (VRMS)</th>
-				</tr>
-			   </thead>
-			</table>
+			  		<th>Current needed (mA)</th>
+			  		<th>Power Needed (mW)</th>
+				  </tr>
+			     </thead>
+			    <tbody>
+			    <tr>
+			      <td>Safe</td>
+			      <td>85</td>
+			      <td>{this.props.voltage_safe}</td>
+			      <td>{this.props.current_safe}</td>
+			      <td>{this.props.power_safe}</td>
+			    </tr>
+			    <tr>
+			      <td>Moderate</td>
+			      <td>100</td>
+			      <td>{this.props.voltage_moderate}</td>
+			      <td>{this.props.current_moderate}</td>
+			      <td>{this.props.power_moderate}</td>
+			    </tr>
+			    <tr>
+			      <td>Fairly Loud</td>
+			      <td>110</td>
+			      <td>{this.props.voltage_fairlyloud}</td>
+			      <td>{this.props.current_fairlyloud}</td>
+			      <td>{this.props.power_fairlyloud}</td>
+			    </tr>
+			    <tr>
+			      <td>Very Loud</td>
+			      <td>115</td>
+			      <td>{this.props.voltage_veryloud}</td>
+			      <td>{this.props.current_veryloud}</td>
+			      <td>{this.props.power_veryloud}</td>
+			    </tr>
+			    <tr>
+			      <td>Painfully Loud</td>
+			      <td>120</td>
+			      <td>{this.props.voltage_painful}</td>
+			      <td>{this.props.current_painful}</td>
+			      <td>{this.props.power_painful}</td>
+			    </tr>
+			    </tbody>
+			  </table>
 			</div>
 		);
 	}
